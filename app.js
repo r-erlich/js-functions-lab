db0cca1c305e3c1bf019fd3e03b2164c4fec245a
Reuben <grerlich@gmail.com>
2024-09-13 11:33:08 -0400

    finished lab

--- app.js
+++ app.js
@@ -53,4 +53,104 @@ const isCharAVowel = (vowel) => {
     return false;
   }
 };
-console.log("Exercise 3 Result:", isCharAVowel("I"));
+console.log("Exercise 3 Result:", isCharAVowel("a"));
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+const generateEmail = (name, domain) => {
+  return `${name}@${domain}`;
+};
+console.log("Exercise 4 Result:", generateEmail("reuben", "reuben.com"));
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+const greetUser = (name, time) => {
+  return `Good ${time}, ${name}!`;
+};
+
+console.log("Exercise 5 Result:", greetUser("reuben", "evening"));
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+const maxOfThree = (x, y, z) => {
+  if (x >= y && x >= z) {
+    return x;
+  } else if (y >= x && y >= z) {
+    return y;
+  } else {
+    return z;
+  }
+};
+
+console.log("Exercise 6 Result:", maxOfThree(1, 2, 3));
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+const calculateTip = (bill, tip) => {
+  return (bill * tip) / 100;
+};
+
+console.log("Exercise 7 Result:", calculateTip(50, 20));
+
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+const convertTemperature = (temperature, scale) => {
+  if (scale === "C") {
+    return (temperature * 9) / 5 + 32;
+  } else if (scale === "F") {
+    return ((temperature - 32) * 5) / 9;
+  }
+};
+
+console.log("Exercise 8 Result:", convertTemperature(32, "F"));
